ee74fc17776e810576d9ded25cefe9e3cd7728e4
Fredo Guan <fredo.guan@hotmail.com>
2023-04-18 17:54:38 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -163,8 +163,8 @@ def getData():
     #trainSet = torchvision.datasets.ImageNet(FLAGS['imageRoot'], split = 'train')
     #testSet = torchvision.datasets.ImageNet(FLAGS['imageRoot'], split = 'val')
     
-    trainSet = timm.data.create_dataset('', root=FLAGS['imageRoot'], split = 'train', batch_size=FLAGS['batch_size'])
-    testSet = timm.data.create_dataset('', root=FLAGS['imageRoot'], split = 'validation', batch_size=FLAGS['batch_size'])
+    trainSet = timm.data.create_dataset('all', root=FLAGS['imageRoot'], split = 'train', batch_size=FLAGS['batch_size'])
+    testSet = timm.data.create_dataset('all', root=FLAGS['imageRoot'], split = 'validation', batch_size=FLAGS['batch_size'])
 
     global classes
     classes = {classIndex : className for classIndex, className in enumerate(trainSet.classes)}
